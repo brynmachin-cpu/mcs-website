c7a79071569056f760b9084e7c2455f92ba5701b
Bryn Machin <bryn@mcs.gi>
2026-04-28 16:10:24 +0200

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import MachinConsultingWebsite from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MachinConsultingWebsite />
+    <>
+      <MachinConsultingWebsite />
+      <Analytics />
+    </>
   </React.StrictMode>
 );
