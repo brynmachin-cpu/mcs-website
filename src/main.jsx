23ce1bbc971c8401924b0626d9a5b2a68cc43753
Bryn Machin <bryn@mcs.gi>
2026-05-04 14:46:42 +0200

    Add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 import MachinConsultingWebsite from "./App.jsx";
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")).render(
     <>
       <MachinConsultingWebsite />
       <Analytics />
+      <SpeedInsights />
     </>
   </React.StrictMode>
 );
